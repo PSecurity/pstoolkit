21ee70c730c2c9b5d2c876e2c04e286351e2e910
Gilberto Martins <45055662+PSecurity@users.noreply.github.com>
2026-05-02 02:03:24 -0300

    Update fire.sh

--- modules/fire.sh
+++ modules/fire.sh
@@ -1,1 +1,19 @@
+fire_menu() {
+echo "🔥 Fire Toolkit"
+echo "1) HTTP Server (Python)"
+echo "2) File Share rápido"
 
+read -p "Escolha: " f
+
+case $f in
+1)
+	echo "Iniciando server..."
+	cd /sdcard
+	python3 -m http.server 8080
+	;;
+2)
+	echo "Link local:"
+	ip addr show wlan0 | grep inet
+	;;
+esac
+}
