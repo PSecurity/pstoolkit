b905b6973e74056d7b26f982977fa10abceee581
Gilberto Martins <45055662+PSecurity@users.noreply.github.com>
2026-05-02 02:32:32 -0300

    Update ngrok.sh

--- modules/ngrok.sh
+++ modules/ngrok.sh
@@ -1,17 +1,11 @@
-ngrok_menu() {
-echo "🚀 Ngrok Toolkit"
-echo "1) Start HTTP tunnel"
-echo "2) Start custom port"
-read -p "Escolha: " n
+ngrok_module() {
 
-case $n in
-1)
-	sudo_mode "pkill ngrok"
-	sudo_mode "./ngrok http 8080"
-	;;
-2)
-	read -p "Porta: " p
-	sudo_mode "./ngrok http $p"
-	;;
-esac
+echo "🚀 Iniciando CORE PS.NGROK..."
+
+cd $HOME/PeekToolkit/ps.ngrok || {
+	echo "❌ ps.ngrok não encontrado"
+	return
+}
+
+bash ngrok.sh
 }
