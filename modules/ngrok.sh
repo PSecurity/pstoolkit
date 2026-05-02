a9f55a260ad6d65ab9805fe894b88438f06407f6
Gilberto Martins <45055662+PSecurity@users.noreply.github.com>
2026-05-02 02:02:56 -0300

    Update ngrok.sh

--- modules/ngrok.sh
+++ modules/ngrok.sh
@@ -1,1 +1,17 @@
+ngrok_menu() {
+echo "🚀 Ngrok Toolkit"
+echo "1) Start HTTP tunnel"
+echo "2) Start custom port"
+read -p "Escolha: " n
 
+case $n in
+1)
+	sudo_mode "pkill ngrok"
+	sudo_mode "./ngrok http 8080"
+	;;
+2)
+	read -p "Porta: " p
+	sudo_mode "./ngrok http $p"
+	;;
+esac
+}
