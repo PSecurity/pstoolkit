6c0aa78dba855ab5b39275a87435ef515472032a
Gilberto Martins <45055662+PSecurity@users.noreply.github.com>
2026-05-02 02:03:06 -0300

    Update cloud.sh

--- modules/cloud.sh
+++ modules/cloud.sh
@@ -1,1 +1,21 @@
 
+cloud_menu() {
+echo "☁️ Cloud Toolkit"
+echo "1) Backup Termux"
+echo "2) Sync pasta Downloads"
+
+read -p "Escolha: " c
+
+case $c in
+1)
+	sudo_mode "termux-setup-storage"
+	cp -r $HOME $HOME/cloud_backup
+	echo "Backup feito"
+	;;
+2)
+	sudo_mode "termux-setup-storage"
+	cp -r /sdcard/Download $HOME/cloud_sync
+	echo "Sync concluído"
+	;;
+esac
+}
